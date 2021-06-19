0ad7769eea91be5cd67d267929fe0ed4983f62b7
gwc0111 <gwc0111@qq.com>
2021-06-19 10:20:09 +0800

    添加相机边界限制，添加隐藏提示prefab

--- Assets/Scripts/Tips.cs
+++ Assets/Scripts/Tips.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Fungus;
 using UnityEngine;
 
 public class Tips : MonoBehaviour
 {
+    [SerializeField] string block;
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.gameObject.tag == "Player")
+        {
+            var flowchart = FindObjectOfType<Flowchart>();
+            if(flowchart != null)
+            {
+                flowchart.ExecuteBlock(block);
+            }
+        }
 
     }
 }
