e3f026eb72d2069f50907e2fe0dd5bf72c7e3895
Duanzhiyong520 <386104554@qq.com>
2021-06-19 14:14:30 +0800

    修改objbase

--- Assets/Scripts/ObjBase.cs
+++ Assets/Scripts/ObjBase.cs
@@ -1,15 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ObjBase : MonoBehaviour
+public class ObjBase : MonoBehaviour, IPointerClickHandler
 {
+    public GameObject CurObj;
+    private Camera m_Camera;
+    public bool IsSelectObj = false;
     private void Awake()
     {
+        m_Camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        CurObj = gameObject;
         Init();
     }
     public virtual void Init()
     { }
     public virtual void Play()
     { }
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+
+        Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            GameObject gameobj = hit.collider.gameObject;
+            
+                IsSelectObj = true;
+            
+            
+            OnUpdate(hit);
+        }
+    }
+    public virtual void OnUpdate(RaycastHit hit)
+    {
+
+    }
+    private void OnMouseExit()
+    {
+        IsSelectObj = false;
+    }
+    private void OnMouseDown()
+    {
+
+        OnObjMouseDown();
+    }
+    public virtual void OnObjMouseDown() { }
+    private void OnMouseUp()
+    {
+        OnObjMouseUp();
+    }
+    private void OnMouseEnter()
+    {
+        OnObjMouseEnter();
+    }
+    public virtual void OnObjMouseEnter() { }
+    public virtual void OnObjMouseUp() { }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.LogError("onpointclick");
+    }
+    public void OnObjMouseUp(PointerEventData eventData)
+    {
+        Debug.LogError("onpointclick");
+    }
 }
--- Assets/Scripts/PhotoObj.cs
+++ Assets/Scripts/PhotoObj.cs
@@ -1,26 +1,78 @@
-﻿using System.Collections;
+﻿using Fungus;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 public class PhotoObj : ObjBase
 {
+    enum TutorialStep
+    {
+        None,
+        Forcus,
+        Disapera,
+        Reset,
+    }
+    private TutorialStep mStep = TutorialStep.None;
     private Transform mPhoto;
     private Transform mKey;
+    private bool mStartNextStep = false;
+    private Flowchart mFlowChat;
+
+    private bool mIsCanOperate = true;
     //private BoxCollider2D mPhotoBox;
     //private BoxCollider2D mKeyBox;
     public override void Init()
     {
         mPhoto = transform.Find("Photo");
         mKey = transform.Find("Key");
+        mFlowChat = GameObject.Find("Flowchart").GetComponent<Flowchart>();
         //mPhotoBox = mPhoto.GetComponent<BoxCollider2D>();
         //mKeyBox = mKey.GetComponent<BoxCollider2D>();
-
-    }
-    void Update()
-    {
         
     }
+    public override void OnObjMouseDown()
+    {
+        if(!mIsCanOperate)
+        {
+            return;
+        }
+        mStep = (TutorialStep)(mStep + 1);
+        Debug.LogWarning("mStep : " + mStep);
+        mStartNextStep = true;
+    }
+    
+    public override void OnObjMouseUp()
+    {
+        mStartNextStep = false;
+    }
+    public override void OnUpdate(RaycastHit hit)
+    {
+        if(mStartNextStep)
+        {
+            mStartNextStep = false;
+            if(mStep == TutorialStep.Forcus)
+            {
+                 mFlowChat.GetExecutingBlocks();
+                mFlowChat.ExecuteBlock("ViewPhoto");
+                StartCoroutine(WaitSeond());
+            }
+        }
+    }
+
+    private IEnumerator WaitSeond()
+    {
+        mIsCanOperate = false;
+        yield return new WaitForSeconds(1);
+        mIsCanOperate = true;
+    }
+
+    private void ForcusStep()
+    {
+
+    }
+
     public override void Play()
     {
 
