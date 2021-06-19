10e5efcc3df3d343210c189fc789bfdaadc63e82
gwc0111 <gwc0111@qq.com>
2021-06-19 09:47:05 +0800

    补传文件

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] int speed = 5;
+    Animator animator;
+    SpriteRenderer sp;
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    { 
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (Mathf.Approximately(horizontal, 0.0f))
+        {
+            return;
+        }
+
+        sp.flipX = horizontal < 0 ? true : false;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        float dmove = speed * horizontal * Time.deltaTime;
+        rigidbody2d.position = new Vector2(rigidbody2d.position.x+dmove, rigidbody2d.position.y);
+    }
+
+}
