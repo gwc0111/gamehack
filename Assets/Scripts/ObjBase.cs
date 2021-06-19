521fd71399609f7465f441fde9a4e43b512f42c5
Duanzhiyong520 <386104554@qq.com>
2021-06-19 10:50:32 +0800

    搭建第二关

---
+++ Assets/Scripts/ObjBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjBase : MonoBehaviour
+{
+    private void Awake()
+    {
+        Init();
+    }
+    public virtual void Init()
+    { }
+    public virtual void Play()
+    { }
+}
---
+++ Assets/Scripts/PhotoObj.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+public class PhotoObj : ObjBase
+{
+    private Transform mPhoto;
+    private Transform mKey;
+    //private BoxCollider2D mPhotoBox;
+    //private BoxCollider2D mKeyBox;
+    public override void Init()
+    {
+        mPhoto = transform.Find("Photo");
+        mKey = transform.Find("Key");
+        //mPhotoBox = mPhoto.GetComponent<BoxCollider2D>();
+        //mKeyBox = mKey.GetComponent<BoxCollider2D>();
+
+    }
+    void Update()
+    {
+        
+    }
+    public override void Play()
+    {
+
+    }
+}
