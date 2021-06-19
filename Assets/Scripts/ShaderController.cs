7425bf575a776dc08159ee637a6f828776876a43
gwc0111 <gwc0111@qq.com>
2021-06-19 09:03:54 +0800

    文件路径调整

---
+++ Assets/Scripts/ShaderController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+    Material material;
+    float fade = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    //图像完整是1，图像完全消失是0
+    public void SetDissolve(float fade)
+    {
+        material.SetFloat("_Fade", fade);
+    }
+}
