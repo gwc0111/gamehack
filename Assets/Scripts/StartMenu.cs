6d35165366cb4d5aa3bbc3b8e62fd1cefe314d10
Duanzhiyong520 <386104554@qq.com>
2021-06-20 12:00:30 +0800

    增加资源

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button StartBtn;
+    public Button ExitBtn;
+    public GameObject MenuPrs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartBtn.onClick.AddListener(OnStartBtn);
+        ExitBtn.onClick.AddListener(OnExitBtnClick);
+    }
+
+    private void OnStartBtn()
+    {
+        Debug.LogError("OnStart");
+
+    }
+    private void OnExitBtnClick()
+    {
+        Application.Quit();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
